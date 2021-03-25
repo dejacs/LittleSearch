e18077fc7c1c95c45c26ef4139880fd560921080
Jade Silveira <jadecs8@gmail.com>
2021-03-25 09:16:27 -0300

    [ADD] pod

---
+++ Pods/Target Support Files/Pods-LittleSearch-LittleSearchUITests/Pods-LittleSearch-LittleSearchUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LittleSearch_LittleSearchUITests : NSObject
+@end
+@implementation PodsDummy_Pods_LittleSearch_LittleSearchUITests
+@end
---
+++ Pods/Target Support Files/Pods-LittleSearch-LittleSearchUITests/Pods-LittleSearch-LittleSearchUITests-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_LittleSearch_LittleSearchUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_LittleSearch_LittleSearchUITestsVersionString[];
+
